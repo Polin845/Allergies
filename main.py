5f6f06a51c5db8c17d50f486b9ab091f93ed2546
Rusty_cube <128138709+Polin845@users.noreply.github.com>
2023-11-17 08:50:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 from data.base import Log
+from forms.log_on import LoginForm
 from flask_login import LoginManager, login_user
+import os
+
+SECRET_KEY = os.urandom(32)
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def ajr():
     return render_template('a.html')
 
-@app.route('/log_on')
+@app.route('/log_on', methods=["POST", "GET"])
 def loginig():
-    return render_template('l_on.html')
+    form = LoginForm()
+    return render_template("l_on.html", title="Авторизация", form=form)
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
