7a9af7e435e05aa931c97fcfc786190b4a6b5ffc
Rusty_cube <128138709+Polin845@users.noreply.github.com>
2023-11-18 19:12:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data.base import Log
-from forms.log_on import LoginForm
+from forms.register import LoginForm
 from flask_login import LoginManager, login_user
 import os
 
@@ -14,10 +14,17 @@ app.config['SECRET_KEY'] = SECRET_KEY
 def ajr():
     return render_template('a.html')
 
-@app.route('/log_on', methods=["POST", "GET"])
-def loginig():
+@app.route('/registration', methods=["POST", "GET"])
+def register():
     form = LoginForm()
-    return render_template("l_on.html", title="Авторизация", form=form)
+    if request.method == 'POST':
+
+        try:
+            Log(request.form['login'], request.form['email'],
+                request.form['psw1'], request.form['all'])
+        except:
+            Log(error=1)
+    return render_template("l_on.html", title="Авторизация", form=form, error=0)
 
 if __name__ == '__main__':
     app.run(debug=True)
