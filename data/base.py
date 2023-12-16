0dda3872d15d8ddc141405c747b9e2e269ae97a6
Rusty_cube <128138709+Polin845@users.noreply.github.com>
2023-12-16 15:41:27 +0300

    Add files via upload

--- data/base.py
+++ data/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import Column, Integer, String
 
 # строка подключения
-sqlite_database = "sqlite:///metanit.db"
+sqlite_database = "sqlite:///meta.db"
 # создаем движок SqlAlchemy
 engine = create_engine(sqlite_database, echo=True)
 
@@ -26,11 +26,21 @@ class Person(Base):
 Base.metadata.create_all(bind=engine)
 
 # создаем сессию подключения к бд
-def Log(log, em, pas, al, error=0):
+def Per(log, pas, func, em='', al=''):
     with Session(autoflush=False, bind=engine) as db:
-        if error:  #проверяем ошибки
-            db.rollback()
-        # создаем объект Person для добавления в бд
-        person = Person(email=em, login=log, pasword=pas, allergens=al)
-        db.add(person)  # добавляем в бд
-        db.commit()  # сохраняем изменения
+        if func == 'creat':
+            # создаем объект Person для добавления в бд
+            person = Person(email=em, login=log, pasword=pas, allerg='')
+            db.add(person)  # добавляем в бд
+            db.commit()  # сохраняем изменения
+        elif func == 'check':
+            x = db.query(Person).get(log)
+            if x.pasword == pas:
+                return True
+            return False
+        elif func == 'change':
+            x = db.query(Person).filter(Person.login == log).first()
+            x.allerg=al
+            db.commit()
+        elif func == 'ret':
+            return db.query(Person).get(log).allerg
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from data.base import Log
+from flask import Flask, render_template, request, redirect, url_for
+from data.base import Per
 from photo import found
 from flask_login import LoginManager, login_user
 import os
@@ -10,24 +10,49 @@ SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route('/')
-def ajr():
-    return render_template('a.html')
 
-@app.route('/registration', methods=["POST", "GET"])
-def register():
+@app.route('/', methods=["POST", "GET"])
+def lobby():
    if request.method == 'POST':
-        a, image = request.form['all'], request.form['image']
+        a = request.form['all']
+        image = request.files['file']
         if a != '':
             f = found(image, a)
             if f != []:
-                return render_template("l_on.html", result=f)
-        Log(log=request.form['login'], em=request.form['email'],
-            pas=request.form['psw1'], al=a)
-        return render_template("l_on.html", title="Авторизация")
+                return render_template("lobby.html", result=f)
+        return render_template("lobby.html")
    else:
-        return render_template('l_on.html')
+        return render_template('lobby.html')
 
+
+@app.route('/registration', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        log, em, psw1, psw2 = request.form['login'], request.form['email'], request.form['psw1'], request.form['psw2']
+        Per(log=log, em=em, pas=psw1, func='creat')
+        return render_template('main.html')
+    return render_template('registration.html', title='Регистрация')
+
+log, psw = '', ''
+@app.route('/log_in', methods=['POST', 'GET'])
+def log_in():
+    if request.method == 'POST':
+        global log, psw
+        log, psw = request.form['login'], request.form['psw']
+        flag = Per(log=log, pas=psw, func='check')
+        if flag:
+            return render_template('main.html')
+        else:
+            return render_template('log_in.html', massage='1')
+    return render_template('log_in.html')
+
+@app.route('/main', methods=['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        a = request.form['all']
+        Per(log=log, pas=psw, al=a, func='change')
+        render_template('main.html', allergs=a)
+    render_template('main.html', allergs=Per(log=log, pas=psw, func='ret'))
 
 if __name__ == '__main__':
     app.run(debug=True)
